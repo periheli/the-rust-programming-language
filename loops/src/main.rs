44f69c8f701a73536e746a7b31d80a5335d57b6b
peri <76472728+periheli@users.noreply.github.com>
2025-06-01 16:46:24 +0700

    progress: part 3

---
+++ loops/src/main.rs
@@ -0,0 +1,64 @@
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    nested_loops();
+
+    while_loop();
+
+    for_loop();
+}
+
+fn nested_loops() {
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
+
+fn while_loop() {
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    for number in (1..3).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+}
